5dbdc79b7ff11807ff42ef8cabb9dd11d0aefc2f
haoz20 <zhaonianhao2005@gmail.com>
2025-07-05 09:48:05 +0700

    implement data source and delegate

--- Meal Planner/ViewController.swift
+++ Meal Planner/ViewController.swift
@@ -7,14 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    @IBOutlet weak var tableVIew: UITableView!
-    
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
     }
 
 
