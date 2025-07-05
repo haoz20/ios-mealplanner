152bd825911b72c962c386b98d3bcd8c84ffa301
haoz20 <zhaonianhao2005@gmail.com>
2025-07-05 09:48:38 +0700

    add MealTableViewCell

---
+++ Meal Planner/MealTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MealTableViewCell.swift
+//  Meal Planner
+//
+//  Created by Swan Htet Aung on 5/7/25.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
