88c3e19e34c543edbd0024a6bb103059f561cfec
haoz20 <zhaonianhao2005@gmail.com>
2025-07-05 11:37:13 +0700

    implement logic in AddMealViewController.swift

---
+++ Meal Planner/AddMealViewController.swift
@@ -0,0 +1,49 @@
+//
+//  AddMealViewController.swift
+//  Meal Planner
+//
+//  Created by Swan Htet Aung on 5/7/25.
+//
+
+import UIKit
+
+class AddMealViewController: UIViewController {
+    
+    
+    @IBOutlet weak var mealTextField: UITextField!
+    @IBOutlet weak var typeSelection: UISegmentedControl!
+    @IBOutlet weak var isVegetarian: UISwitch!
+    @IBOutlet weak var date: UIDatePicker!
+    @IBOutlet weak var noteTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.title = "Add Meal"
+        navigationItem.rightBarButtonItem =
+        UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(saveMeal))
+        
+    }
+    
+    @objc func saveMeal() {
+        let title = mealTextField.text ?? ""
+        let isVeg = isVegetarian.isOn
+        let note = noteTextView.text ?? ""
+        let date = date.date
+        var type = MealType.breakfast
+        
+        if typeSelection.selectedSegmentIndex == 0 {
+            type = MealType.breakfast
+        } else if typeSelection.selectedSegmentIndex == 1 {
+            type = MealType.lunch
+        } else if typeSelection.selectedSegmentIndex == 2 {
+            type = MealType.dinner
+        }
+        
+        let newMeal = Meal(title: title, type: type, isVegetarian: isVeg, description: note)
+    
+    }
+    
+        
+}
