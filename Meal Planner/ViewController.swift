26f7765fc4c6926eb10df82bfc980e18f150594a
haoz20 <zhaonianhao2005@gmail.com>
2025-07-05 09:16:22 +0700

    Initial Commit

---
+++ Meal Planner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meal Planner
+//
+//  Created by Swan Htet Aung on 5/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
