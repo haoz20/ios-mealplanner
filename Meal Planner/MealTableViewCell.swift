4c717e34359ea0600517931f16bd30af258135fb
haoz20 <zhaonianhao2005@gmail.com>
2025-07-05 10:05:44 +0700

    implement logics in MealTableViewCell

--- Meal Planner/MealTableViewCell.swift
+++ Meal Planner/MealTableViewCell.swift
@@ -8,10 +8,41 @@
 import UIKit
 
 class MealTableViewCell: UITableViewCell {
-
+    
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var noteLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        typeLabel.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        titleLabel.font = UIFont.systemFont(ofSize: 24)
+        noteLabel.font = UIFont.systemFont(ofSize: 16)
     }
 
+    func setup(meal: Meal) {
+        
+        var vegEmoji = ""
+        
+        if meal.isVegetarian {
+            vegEmoji = "🌱"
+        }
+        
+        
+        titleLabel.text = meal.title + vegEmoji
+        noteLabel.text = meal.description
+        switch meal.type {
+        case .breakfast:
+            typeLabel.text = "Breakfast"
+            typeLabel.textColor = .systemOrange
+        case .lunch:
+            typeLabel.text = "Lunch"
+            typeLabel.textColor = .systemGreen
+        case .dinner:
+            typeLabel.text = "Dinner"
+            typeLabel.textColor = .systemBlue
+        }
+    }
+    
 }
