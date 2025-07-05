66897dfe9de08e3caf7aef217aacf1b53dca7eac
haoz20 <zhaonianhao2005@gmail.com>
2025-07-05 09:40:29 +0700

    add classes in Meal.swift

--- Meal Planner/Meal.swift
+++ Meal Planner/Meal.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+enum MealType {
+    case breakfast
+    case lunch
+    case dinner
+}
+
+class DayMeal {
+    var date: Date
+    var meals: [Meal]
+    
+    init(date: Date, meals: [Meal]) {
+        self.date = date
+        self.meals = meals
+    }
+    
+}
+
+
+class Meal {
+    var title: String
+    var type: MealType
+    var isVegetarian: Bool
+    var description: String
+    
+    init(title: String, type: MealType, isVegetarian: Bool, description: String) {
+        self.title = title
+        self.type = type
+        self.isVegetarian = isVegetarian
+        self.description = description
+    }
+    
+}
