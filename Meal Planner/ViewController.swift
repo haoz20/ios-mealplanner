fdb2229a25bcdbe22570eade5466a32e05debf00
haoz20 <zhaonianhao2005@gmail.com>
2025-07-05 10:54:41 +0700

    add test data and add delete swipe actions

--- Meal Planner/ViewController.swift
+++ Meal Planner/ViewController.swift
@@ -11,6 +11,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBOutlet weak var tableView: UITableView!
     
+    var dayMeals: [DayMeal] = [
+        DayMeal(date: Date(), meals:[
+            Meal(title: "Beef Steak", type: .breakfast, isVegetarian: false, description: "Beef steak at countryside steakhouse"),
+            Meal(title: "Tomyum Noodle", type: .lunch, isVegetarian: true, description: "Vegetarian tomyum noodle with bean sprouts")
+        ])
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -20,15 +27,41 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return dayMeals.count
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return dayMeals[section].meals.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "mealcell") as? MealTableViewCell{
+            let meal = dayMeals[indexPath.section].meals[indexPath.row]
+            cell.setup(meal: meal)
+            return cell
+        }
         return UITableViewCell()
+        
     }
-
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 110
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return dayMeals[section].date.formatted(date: .long, time: .omitted)
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") {_, _, completion in
+            self.dayMeals[indexPath.section].meals.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            
+        }
+        return UISwipeActionsConfiguration(actions: [deleteAction])
+    }
 
 }
 
